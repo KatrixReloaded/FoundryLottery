dd6dc4a6ed1987a6a981a3702dff89ccea67d664
KatrixReloaded <kartikeyasureka@gmail.com>
2024-09-09 13:39:36 +0530

    smart contract complete

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -201,4 +201,8 @@ contract Raffle is VRFConsumerBaseV2Plus {
     function getRaffleState() external view returns (RaffleState) {
         return s_raffleState;
     }
+
+    function getPlayer(uint256 index) external view returns(address) {
+        return s_players[index];
+    }
 }
